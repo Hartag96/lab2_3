590f01980d6799dc4cdc6199867e9060ffa2cfb9
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-04-15 20:10:29 +0200

    test calculateJackardSimilarityWhenOneSeqIsEmpty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -24,4 +24,11 @@ public class SimilarityFinderTest {
 
         Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.equalTo(1.0));
     }
+
+    @Test public void calculateJackardSimilarityWhenOneSeqIsEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = {1, 2, 3};
+
+        Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.equalTo(0.0));
+    }
 }
